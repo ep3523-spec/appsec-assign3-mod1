6903530acd201d4667d26ad0ba2c1a0a78c08b5c
Eesha <eesha@nyu.edu>
2025-11-26 17:35:49 -0500

    fix: formatting and trivyignore line endings

--- GiftcardSite/GiftcardSite/settings.py
+++ GiftcardSite/GiftcardSite/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Enable PyMySQL as MySQL driver
 try:
-    import pymysql
+    import pymysql  # type: ignore
     pymysql.install_as_MySQLdb()
 except ImportError:
     pass
